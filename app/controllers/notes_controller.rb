ee80d88a40ae922128675dbdfc0aa53564f06b5e
Layne Rasmussen <layrasm@gmail.com>
2020-08-25 08:52:16 -0600

    controller and route edits

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,17 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+end
